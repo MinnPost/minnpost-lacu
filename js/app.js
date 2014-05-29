5b1bacbd6d215b68c2a09986abb1a05ba16fd4e9
Alan Palazzolo <apalazzolo@minnpost.com>
2014-05-29 13:46:27 -0500

    Placeholder.

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ var svgCanvas;
 var lakeGroup;
 var width = 900;
 var height = 600;
+var localStoragePrefix = 'lacu-';
 
 
 // Let's get our massive dataset first
@@ -27,6 +28,11 @@ function getReady(error, data) {
 
 // Some processing and setting up
 function sett() {
+  // Center canvas in window
+  d3.select('#application-container')
+    .style('top', ((window.innerHeight - height) / 2) + 'px')
+    .style('left', ((window.innerWidth - width) / 2) + 'px');
+
   // Draw canvas
   svgCanvas = d3.select('#application-container').append('svg')
     .attr('width', width)
@@ -43,22 +49,33 @@ function sett() {
 function go() {
   var path = d3.geo.path()
     .projection(null);
+  var lakeIndex = getLakeIndex();
 
-  var lakeIndex = 0;
-
+  // Function for looping
   function showLake() {
+    setLakeIndex(lakeIndex);
     var lakeData = lakes[lakeIndex];
+
+    // Make sure we have data
+    if (!lakeData) {
+      return;
+    }
+
+    // Get parts we will need
     var lakePath = topojson.feature(lakeData, lakeData.objects.l);
     var lake = lakeGroup.select('.lake-outline');
 
+    // Ensure that there is a path in the first place
     if (!lake || !lake[0][0]) {
       lake = lakeGroup.append('path').attr('class', 'lake-outline');
     }
 
+    // If not path set, then make this one the first
     if (!lake.attr('d')) {
       lake.attr('d', path(lakePath));
     }
 
+    // Transition to new lake
     lake
       .transition().duration(500)
         .attr('d', path(lakePath))
@@ -69,6 +86,32 @@ function go() {
       });
   }
 
+  // Start the magic
   showLake();
+}
 
+// Get lake index
+function getLakeIndex() {
+  // Store index locally, so we don't have to start over
+  // every reload
+  var index = localStorage.getItem(localStoragePrefix + '-index');
+  if (index) {
+    return parseInt(index, 10);
+  }
+  else {
+    setLakeIndex(0);
+    return 0;
+  }
+}
+
+// Set lake index
+function setLakeIndex(index) {
+  localStorage.setItem(localStoragePrefix + '-index', index);
+  return index;
+}
+
+// Reset lake index
+function resetLakeIndex() {
+  localStorage.removeItem(localStoragePrefix + '-index');
+  return 0;
 }
