c62035264f285fea067de5472da18dd5b980892d
Alan Palazzolo <apalazzolo@minnpost.com>
2014-06-14 02:12:00 -0500

    Adding in PCA data.

--- data-processing/process-lakes.js
+++ data-processing/process-lakes.js
@@ -6,6 +6,7 @@
 // Modules
 var path = require('path');
 var fs = require('fs');
+var _ = require('lodash');
 var shapefile = require('shapefile');
 var topojson = require('topojson');
 var mergeProperties = require('topojson/lib/topojson/merge-properties');
@@ -21,6 +22,15 @@ var pcaTSIData = require('../data/build/pca_summary_tsi.json');
 var pcaTransparencyData = require('../data/build/pca_transparency_trends.json');
 var pcaWaterData = require('../data/build/pca_water_units.json');
 var finalLakes = {};
+
+// Transparency data is not keyed as there is streams and stations in
+// the data, so we group it by ID
+pcaTransparencyData = _.groupBy(pcaTransparencyData, 'WID');
+
+// Get list of Watersheds
+var watersheds = _.sortBy(_.uniq(_.map(pcaWaterData, function(w, wi) {
+  return w.WATERSHED_NAME;
+})));
 
 // Configuration
 var canvasWidth = 1400;
@@ -63,6 +73,65 @@ function transformProperties(feature) {
       return (!p) ? c : (p.length >= c.length) ? c : p;
     }, '');
   p.n = (p.n.toLowerCase().match(regexSubName)) ? p.n.toLowerCase().match(regexSubName)[1] : p.n;
+
+  // Add PCA data
+  p.ids.forEach(function(i, ii) {
+    var pcaID = i.substring(0, 2) + '-' + i.substring(2, 6) + '-' + i.substring(6, 8);
+
+    // Condition summaries
+    var cs = pcaConditionData[pcaID];
+    if (cs && cs.SUMMARY_STRING) {
+      cs = cs.SUMMARY_STRING;
+      p.nswim = (cs.match(/Not always suitable for swimming and wading/i) !== null) ? true : '';
+      p.swim = (cs.match(/Suitable for swimming and wading,/i) !== null) ? true : '';
+      p.bqual = (cs.match(/exceed the water quality standard/i) !== null) ? true : '';
+      p.nfish = (cs.match(/May not support a thriving community of fish and other aquatic organisms/i) !== null) ? true : '';
+      p.fish = (cs.match(/Available data indicate a thriving community of fish and other aquatic organisms/i) !== null) ? true : '';
+    }
+
+    // TSI (Trophic State Index)
+    // http://www.lakeaccess.org/lakedata/datainfotsi.html
+    //
+    // Ecoregions
+    // NLF - Northern Lakes & Forests
+    // NCHF - North Central Hardwood Forests
+    // WCBP - Western Corn Belt Plains
+    // NGP - Northern Glaciated Plains
+    var tsi = pcaTSIData[pcaID];
+    if (tsi && tsi.ECOREGION) {
+      p.r = (tsi.ECOREGION) ? tsi.ECOREGION : '';
+      p.tsi = (parseInt(tsi.OVERALL_TSI, 10)) ? parseInt(tsi.OVERALL_TSI, 10) : '';
+    }
+
+    // Transparency data (grouped)
+    var tr = pcaTransparencyData[pcaID];
+    if (tr && tr.length > 0) {
+      tr.forEach(function(t, ti) {
+        var s = t.TREND_SUMMARY;
+        var m;
+
+        if (s.match(/this is (strong )?evidence/i) !== null) {
+          m = s.match(/at this lake from [0-9]{4} to [0-9]{4} (decreased|increased) by ([0-9.]+) feet per decade/i);
+          p.tchg = parseFloat(m[2]) * ((m[1] === 'decreased') ? -1 : 1);
+        }
+      });
+    }
+
+    // Water unit data
+    var w = pcaWaterData[pcaID];
+    if (w && w.WU_TYPE) {
+      p.mdep = (parseInt(w.LAKE_DEPTH_MAX_FT, 10)) ? parseInt(w.LAKE_DEPTH_MAX_FT, 10) : '';
+
+      // Use class.  Only get number
+      // http://www.pca.state.mn.us/index.php/water/water-permits-and-rules/water-rulemaking/water-quality-standards.html#elements-of-standards
+      p.use = _.map(w.WU_USE_CLASS.split(','), function(u, ui) {
+        u = u.trim();
+        u = u[0];
+        return u;
+      });
+    }
+
+  });
 
   return p;
 }
